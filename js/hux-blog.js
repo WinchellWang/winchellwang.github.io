368b706af085a6e567f6fe731e6ef50c83aee846
MoaxWang <wenqiang.wang@monash.edu>
2022-07-05 22:05:35 +0800

    Update

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -27,21 +27,6 @@
 //  $("img").addClass("img-responsive");
 // });
 
-//百度推广工具
-$(function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-});
-
-
 // responsive tables
 $(document).ready(function() {
     $("table").wrap("<div class='table-responsive'></div>");
@@ -97,18 +82,4 @@ jQuery(document).ready(function($) {
                 }
             });
     }
-});
-
-// add baiduspider tools
-(function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-})();
+})
