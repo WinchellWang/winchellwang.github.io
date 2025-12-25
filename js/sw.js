a3530c23bda8e2e34f7ca878ab71ee50fbb5573e
winchell.sr <winchell.wang@outlook.com>
2025-12-24 19:26:04 -0500

    PWA Comptiable

---
+++ js/sw.js
@@ -0,0 +1,50 @@
+// Service Worker for PWA
+const CACHE_NAME = 'winchell-blog-v1';
+const urlsToCache = [
+  '/',
+  '/css/bootstrap.min.css',
+  '/css/hux-blog.css',
+  '/css/syntax.css',
+  '/js/prism.js',
+  '/img/favicon.ico'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
